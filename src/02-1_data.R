6a1c1b436939c0dba7ec66b9066e2ea846ce507b
Ana Paula Graf <grafapp@gmail.com>
2021-02-02 17:39:40 -0600

    Corrección, se agregó una semilla

--- src/02-1_data.R
+++ src/02-1_data.R
@@ -1,10 +1,13 @@
 # Creamos dos tablas de diferente tamaño
 library(tidyverse)
+
+set.seed(072519)
+
 # El objetivo es correr con sparklyr un inner joint y medir a qué velocidad trabaja
-data.table::fwrite(tibble(x = rnorm(10e3), y = sin(x)), "data/raw/file1.txt")
-data.table::fwrite(tibble(x = rnorm(10e3), y = sin(x)), "data/raw/file2.txt")
+# data.table::fwrite(tibble(x = rnorm(10e3), y = sin(x)), "data/raw/file1.txt")
+# data.table::fwrite(tibble(x = rnorm(10e3), y = sin(x)), "data/raw/file2.txt")
 
 # largefile1 pesa 5.3 GB
-data.table::fwrite(tibble(x = rnorm(15e7), y = sin(x)), "data/raw/largefile1.txt")
-data.table::fwrite(tibble(x = rnorm(10e7), y = sin(x)), "data/raw/largefile2.txt")
+data.table::fwrite(tibble(x = rnorm(15e6), y = sin(x)), "data/raw/largefile1.txt")
+data.table::fwrite(tibble(x = rnorm(10e6), y = sin(x)), "data/raw/largefile2.txt")
 
--- src/02-2_spark_join.R
+++ src/02-2_spark_join.R
@@ -15,7 +15,11 @@ x %>% inner_join(x, by = 'x') %>%
 x1 <- spark_read_csv(sc, "data/raw/largefile1.txt", memory = F)
 x2<- spark_read_csv(sc, "data/raw/largefile2.txt", memory = F)
 
-x1 %>% inner_join(x2, by = 'x') %>% count()
+system.time(
+      n <- x1 %>% 
+        inner_join(x2, by = 'x') %>% 
+        count())
+
 
 
 
